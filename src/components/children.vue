ba72955e5e6ef127ee279f5804b8e4667534cf51
unknown <W9006081@adc.com>
2020-04-02 18:09:51 +0800

    hello

---
+++ src/components/children.vue
@@ -0,0 +1,29 @@
+<template>
+  <div @click="toConfigFn" class="toConfig">
+    这是一个子组件,这是一个按钮
+  </div>
+</template>
+
+<script>
+
+export default {
+  name:'nihao',
+  data() {
+    return {};
+  },
+  methods: {
+      toConfigFn(){
+          console.log('子组件的事件触发了')
+          this.$emit('whereValue',{name:'你少', age:'123456'})
+      }
+  },
+
+};
+</script>
+<style scoped lang="scss">
+.toConfig{
+    width: 300px;
+    height: 300px;
+    background: red;
+}
+</style>
---
+++ src/components/configC.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="clid">这是一个config的子组件BBBBBBBBB</div>
+    <!-- <el-button type="primary" @click="clickEve">点击</el-button> -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.clid{
+    height:400px;
+    background: red;
+}
+</style>
